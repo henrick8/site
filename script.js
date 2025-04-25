54618f18d8e3444c3a269cf5243f7e01b08b8b0c
bah <137510289+henrick8@users.noreply.github.com>
2025-04-25 14:16:00 -0300

    .

--- script.js
+++ script.js
@@ -19,13 +19,13 @@ document.addEventListener('DOMContentLoaded', function(){
      let tamanhoAtualFonte = 1;
  
      aumentaFonteBotao.addEventListener('click', function(){
-         tamanhoAtualFonte += 0.1;
+         tamanhoAtualFonte += 0.2;
          document.body.style.fontSize = `${tamanhoAtualFonte}rem`
  
      })
  
      diminuiFonteBotao.addEventListener('click', function(){
-         tamanhoAtualFonte -= 0.1;
+         tamanhoAtualFonte -= 0.2;
          document.body.style.fontSize = `${tamanhoAtualFonte}rem`
  
      })
